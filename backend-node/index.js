63542d99d653728419876d0696872fb3f0908cde
Anoop Naravaram <anoop901@gmail.com>
2019-03-30 01:44:32 -0700

    Add .gitignore, remove some extraneous logs, add some package-lock.json files.

--- backend-node/index.js
+++ backend-node/index.js
@@ -12,7 +12,6 @@ http.listen(3000, function() {
 
 var midi_input = new midi.input();
 var num_midi_inputs = midi_input.getPortCount();
-console.log(num_midi_inputs);
 for (i = 0; i < num_midi_inputs; i++) {
 	midi_input.openPort(i);
 }
@@ -21,14 +20,9 @@ midi_input.on('message', function(deltaTime, message) {
 	var statusByte = message[0];
 	var dataByte1 = message[1];
 	var dataByte2 = message[2];
-
-	console.log(message);
-
 	if (statusByte === 0x80) {
-		console.log('off');
 		io.emit('noteoff', {note_number: dataByte1, note_velocity: dataByte2});
 	} else if (statusByte === 0x90) {
-		console.log('on');
 		io.emit('noteon', {note_number: dataByte1, note_velocity: dataByte2});
 	}
 });
