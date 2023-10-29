6fa9187e3efcb126066e847e224c592334b38b20
Anoop Naravaram <anoop901@gmail.com>
2023-10-29 14:07:23 -0700

    support multiple inputs simultaneously

--- backend-node/index.js
+++ backend-node/index.js
@@ -10,21 +10,20 @@ http.listen(3000, function () {
   console.log("listening on *:3000");
 });
 
-console.log("searching for midi inputs");
-var midi_input = new midi.input();
-var num_midi_inputs = midi_input.getPortCount();
-for (i = 0; i < num_midi_inputs; i++) {
-  midi_input.openPort(i);
-}
+var num_midi_inputs = new midi.Input().getPortCount();
 console.log(`found ${num_midi_inputs} midi inputs`);
-
-midi_input.on("message", function (deltaTime, message) {
-  var statusByte = message[0];
-  var dataByte1 = message[1];
-  var dataByte2 = message[2];
-  if (statusByte === 0x80) {
-    io.emit("noteoff", { note_number: dataByte1, note_velocity: dataByte2 });
-  } else if (statusByte === 0x90) {
-    io.emit("noteon", { note_number: dataByte1, note_velocity: dataByte2 });
-  }
-});
+for (i = 0; i < num_midi_inputs; i++) {
+  var midi_input = new midi.Input();
+  console.log(`[${i}] ${midi_input.getPortName(i)}`);
+  midi_input.openPort(i);
+  midi_input.on("message", function (deltaTime, message) {
+    var statusByte = message[0];
+    var dataByte1 = message[1];
+    var dataByte2 = message[2];
+    if (statusByte === 0x80) {
+      io.emit("noteoff", { note_number: dataByte1, note_velocity: dataByte2 });
+    } else if (statusByte === 0x90) {
+      io.emit("noteon", { note_number: dataByte1, note_velocity: dataByte2 });
+    }
+  });
+}
